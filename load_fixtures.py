73d2c8f4283c9de280f078a947a93c12aad2ddf3
Kirill-oss67 <Yakovkirill-roslavl@yandex.ru>
2022-06-15 18:45:30 +0300

    0, 1 шаг(доделал)

--- load_fixtures.py
+++ load_fixtures.py
@@ -1,7 +1,11 @@
 from sqlalchemy.exc import IntegrityError
 
 from project.config import DevelopmentConfig
-from project.dao.models import Genre
+from project.dao.models.genre import Genre
+from project.dao.models.director import Director
+from project.dao.models.movie import Movie
+# from project.dao.models.user import User
+
 from project.server import create_app
 from project.setup_db import db
 from project.utils import read_json
@@ -13,6 +17,13 @@ data = read_json("fixtures.json")
 with app.app_context():
     for genre in data["genres"]:
         db.session.add(Genre(id=genre["pk"], name=genre["name"]))
+    for director in data["directors"]:
+        db.session.add(Director(id=director["pk"], name=director["name"]))
+    for movie in data["movies"]:
+        db.session.add(Movie(title=movie["title"], description=movie["description"],
+                                trailer=movie["trailer"], year=movie["year"],
+                                rating=movie["rating"], genre_id=movie["genre_id"],
+                                director_id=movie["director_id"], id=movie["pk"]))
 
     try:
         db.session.commit()
--- project/dao/models/movie.py
+++ project/dao/models/movie.py
@@ -11,9 +11,9 @@ class Movie(BaseMixin, db.Model):
     year = db.Column(db.Integer, nullable=False)
     rating = db.Column(db.Float, nullable=False)
     genre_id = db.Column(db.Integer, db.ForeignKey("genres.id"), nullable=False)
-    genre = db.relationship("Genres")
+    genre = db.relationship("Genre")
     director_id = db.Column(db.Integer, db.ForeignKey("directors.id"), nullable=False)
-    director = db.relationship("Directors")
+    director = db.relationship("Director")
 
     def __repr__(self):
         return f"<Movie '{self.name.title()}'>"
