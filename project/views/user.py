9bc00da34d6c7c0bb7db8f6ca329f238fc5836b4
Kirill-oss67 <Yakovkirill-roslavl@yandex.ru>
2022-06-17 23:24:39 +0300

    4 шаг доделки

--- project/views/user.py
+++ project/views/user.py
@@ -2,17 +2,18 @@ from flask_restx import abort, Namespace, Resource
 from flask import request
 from project.helper import auth_required
 from project.services.user import UserService
+from project.setup_db import db
 
 user_ns = Namespace('user')
 
 
 @user_ns.route('/')
 class UserView(Resource):
-    # @auth_required
+    @auth_required
     def get(self):
         data_tokens = request.headers['Authorization']
         token = data_tokens.split('Bearer')[-1]
-        user = UserService.get_user(token=token, self=UserService)
+        user = UserService(db.session).get_user(token=token)
         return user, 200
 
     @auth_required
@@ -20,7 +21,7 @@ class UserView(Resource):
         data = request.json
         data_tokens = request.headers['Authorization']
         token = data_tokens.split('Bearer')[-1]
-        if UserService.update_user(token=token, data=data, self=UserService):
+        if UserService(db.session).update_user(token=token, data=data):
             return '', 200
         else:
             abort(400)
@@ -34,7 +35,7 @@ class UserView(Resource):
             abort(400)
         password_1 = data['password_1']
         password_2 = data['password_2']
-        if UserService.update_passwords(password_1, password_2, token, self=UserService):
+        if UserService(db.session).update_passwords(password_1, password_2, token):
             return '', 200
         else:
             abort(400)
